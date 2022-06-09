8bb108c3f4e8e10c5f5eeb6c76508f91c146b6b8
Deepak Khadka <deepakkhadka@neputer.com>
2022-06-09 15:05:06 +0545

    chore: include under construction message

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <h2>Under Construction in react js</h2>
+       <h4>Hey it's me Deepak Khadka </h4>
+
+        <h2>Website is under construction in react js</h2>
+
     </div>
   );
 }
