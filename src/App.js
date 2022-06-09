8b40848722358b8a8e37b0061184b8db71d70fb7
Deepak Khadka <deepakkhadka@neputer.com>
2022-06-09 16:15:32 +0545

    chore: include loading spinner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { NavMenu } from './component/navbar';
+import { Spinner} from 'react-bootstrap';
+
 function App() {
   return (
     <div className="App">
@@ -8,6 +10,10 @@ function App() {
        <h4>Hey it's me Deepak Khadka </h4>
 
         <h2>Website is under construction in react js</h2>
+
+        <Spinner animation="border" role="status">
+               <span className="visually-hidden">Loading...</span>
+        </Spinner>
 
     </div>
   );
