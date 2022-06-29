a835b920048acf43a7b068773d7ba0694fda2300
Deepak <rozankhdka159@gmail.com>
2022-06-29 06:36:46 +0545

    chore: include contact us in discord

--- src/component/Blogs.js
+++ src/component/Blogs.js
@@ -4,16 +4,16 @@ import { Link } from "react-router-dom";
 function Blogs() {
     return(
         <>
-           <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-  <div class="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+           <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+  <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
     <div>
-      <p class="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+      <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
        Latest Blogs
       </p>
     </div>
-    <h2 class="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-      <span class="relative inline-block">
-        <svg viewBox="0 0 52 24" fill="currentColor" class="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
+    <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+      <span className="relative inline-block">
+        <svg viewBox="0 0 52 24" fill="currentColor" className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
           <defs>
             <pattern id="db164e35-2a0e-4c0f-ab05-f14edc6d4d30" x="0" y="0" width=".135" height=".30">
               <circle cx="1" cy="1" r=".7"></circle>
@@ -21,26 +21,26 @@ function Blogs() {
           </defs>
           <rect fill="url(#db164e35-2a0e-4c0f-ab05-f14edc6d4d30)" width="52" height="24"></rect>
         </svg>
-        <span class="relative">Lat </span>
+        <span className="relative">Lat </span>
       </span>
        est blogs that can boost your daily activities
     </h2>
-    <p class="text-base text-gray-700 md:text-lg">
+    <p className="text-base text-gray-700 md:text-lg">
       Here are some of the blogs that are related to laravel, react and web hosting
     </p>
   </div>
-  <div class="grid max-w-sm gap-5 mb-8 lg:grid-cols-3 sm:mx-auto lg:max-w-full">
-    <div class="px-10 py-20 text-center border rounded lg:px-5 lg:py-10 xl:py-20">
-      <p class="mb-2 text-xs font-semibold tracking-wide text-gray-600 uppercase">
+  <div className="grid max-w-sm gap-5 mb-8 lg:grid-cols-3 sm:mx-auto lg:max-w-full">
+    <div className="px-10 py-20 text-center border rounded lg:px-5 lg:py-10 xl:py-20">
+      <p className="mb-2 text-xs font-semibold tracking-wide text-gray-600 uppercase">
         28 JUN 2022
       </p>
      
        <Link to="/blogs/hosting-with-cloudfare-and-github" >
-         <span class="inline-block max-w-xs mx-auto mb-3 text-2xl font-extrabold leading-7 transition-colors duration-200 hover:text-deep-purple-accent-400">
+         <span className="inline-block max-w-xs mx-auto mb-3 text-2xl font-extrabold leading-7 transition-colors duration-200 hover:text-deep-purple-accent-400">
         How to Deploy your portfolio with Github pages and Host on Cloudfare
          </span>
        </Link>
-      <span class="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">
+      <span className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">
       <Link to="/blogs/hosting-with-cloudfare-and-github" >
         Read more
         </Link>
--- src/component/contactus.js
+++ src/component/contactus.js
@@ -1,11 +1,81 @@
+import {useState} from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const ContactUs = () => {
-   return (
-       <div className="container">
-           <center>
-          <h3> This is a contact us page </h3>
-           </center>
-       </div>
-   )
+
+    const [email, setEmail] = useState("");
+    const [fullName, setFullName] = useState("");
+    const [message, setMessage] = useState("");
+
+    const sendNotification = () => {
+        if (email && fullName && message) {
+
+            const request = new XMLHttpRequest();
+            request.open("POST", "https://discord.com/api/webhooks/991384867701018684/5Fbfq0E0xO1odBamr7U_TIhYEADMi9BloEnGUnhW5MnyyiqyPkjifgkDKCxI7s5xCbRg");
+
+            request.setRequestHeader('Content-type', 'application/json');
+
+            const params = {
+                username: fullName + " (" + email +")",
+                avatar_url: "",
+                content: message
+            }
+
+            request.send(JSON.stringify(params));
+
+            setFullName("");
+            setEmail("");
+            setMessage("");
+            toast.success("Thank you for your valuable message 🥰")
+
+        } else {
+                toast.error("Please fill valid info")
+        }
+    }
+    return (
+        <div className="bg-gray-800 text-gray-100 px-8 py-12">
+            <div
+                className="max-w-screen-xl mt-10 px-8 grid gap-8 grid-cols-1 md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 py-16 mx-auto bg-gray-100 text-gray-900 rounded-lg shadow-lg">
+                <div className="flex flex-col justify-between">
+                    <div>
+                        <h2 className="text-4xl lg:text-5xl font-bold leading-tight">Lets talk about everything!</h2>
+                        <div className="text-gray-700 mt-8">
+                            Hate forms ? Send us an <span className="underline">email</span> instead.
+                        </div>
+                    </div>
+
+                </div>
+                <div className="">
+                    <div>
+                        <span className="uppercase text-sm text-gray-600 font-bold">Full Name</span>
+                        <input
+                            className="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                            type="text" value={fullName} onChange={(e) => setFullName(e.target.value)}/>
+                    </div>
+                    <div className="mt-8">
+                        <span className="uppercase text-sm text-gray-600 font-bold">Email</span>
+                        <input
+                            className="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                            type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    </div>
+                    <div className="mt-8">
+                        <span className="uppercase text-sm text-gray-600 font-bold">Message</span>
+                        <textarea
+                            className="w-full h-32 bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+                            value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+                    </div>
+                    <div className="mt-8">
+                        <button onClick={sendNotification}
+                                className="uppercase text-sm font-bold tracking-wide bg-indigo-500 text-gray-100 p-3 rounded-lg w-full focus:outline-none focus:shadow-outline">
+                            Send Message
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <ToastContainer />
+        </div>
+    )
 }
 
 export default ContactUs;
