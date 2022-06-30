fff6dea3db4a9a3d45ba07d598be50ea89313507
Deepak <rozankhdka159@gmail.com>
2022-06-30 23:07:02 +0545

    chore: include blog

--- src/component/BlogList/Cloudfare-hosting.js
+++ src/component/BlogList/Cloudfare-hosting.js
@@ -11,97 +11,94 @@ import 'react-medium-image-zoom/dist/styles.css'
 
 const CloudfareHosting = () => {
     return (
-         <>
-           this is a test
-        </>
-        // <section className="bg-white dark:bg-gray-900 container-fluid">
-        //     <div className="container px-6 py-10 mx-auto">
-        //         <h1 className="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">
-        //             How to Deploy your portfolio with Github pages and Host on Cloudfare
-        //         </h1>
-        //         <div className="mt-8 xl:mt-5 lg:flex lg:items-center">
-        //             <div className="w-full grid grid-cols-3 columns-3 gap-8">
+        <section className="bg-white dark:bg-gray-900 container-fluid">
+            <div className="container px-6 py-10 mx-auto">
+                <h1 className="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">
+                    How to Deploy your portfolio with Github pages and Host on Cloudfare
+                </h1>
+                <div className="mt-8 xl:mt-5 lg:flex lg:items-center">
+                    <div className="w-full grid grid-cols-3 columns-3 gap-8">
 
-        //                 <div className="space-y-3">
+                        <div className="space-y-3">
 
-        //                     <h1 className="text-2xl font-semibold text-gray-700 capitalize dark:text-white">
-        //                         <a href="https://github.com">
-        //                             <b>Github Account</b>
-        //                         </a>
-        //                     </h1>
+                            <h1 className="text-2xl font-semibold text-gray-700 capitalize dark:text-white">
+                                <a href="https://github.com">
+                                    <b>Github Account</b>
+                                </a>
+                            </h1>
 
-        //                     <hr/>
-        //                     <p className="text-gray-500 dark:text-gray-300">
-        //                         Step 1: Create your github account.
-        //                     </p>
-        //                     <p className="text-gray-500 dark:text-gray-300">
-        //                         Step 2: Create repository.
-        //                     </p>
-        //                 </div>
+                            <hr/>
+                            <p className="text-gray-500 dark:text-gray-300">
+                                Step 1: Create your github account.
+                            </p>
+                            <p className="text-gray-500 dark:text-gray-300">
+                                Step 2: Create repository.
+                            </p>
+                        </div>
 
-        //                 <div className="space-y-3">
-        //                     <h1 className="text-2xl font-semibold text-gray-700 capitalize dark:text-white">
-        //                         <a href="https://dash.cloudflare.com/"> Cloudfare</a>
-        //                     </h1>
+                        <div className="space-y-3">
+                            <h1 className="text-2xl font-semibold text-gray-700 capitalize dark:text-white">
+                                <a href="https://dash.cloudflare.com/"> Cloudfare</a>
+                            </h1>
 
-        //                     <hr/>
-        //                     <p className="text-gray-500 dark:text-gray-300">
-        //                         Step 1: Create your cloudfare account.
-        //                     </p>
-        //                     <p className="text-gray-500 dark:text-gray-300">
-        //                         Step 2: Create Page.
-        //                         <Zoom>
-        //                             <img src={pageImage} alt="Cloud Fare page"/>
-        //                         </Zoom>
-        //                     </p>
-        //                     <p className="text-gray-500 dark:text-gray-300">
-        //                         Step 3: Create a project.
-        //                         <Zoom>
-        //                             <img src={CreateProjectImage} alt="Create a project"/>
-        //                         </Zoom>
-        //                     </p>
-        //                     <p className="text-gray-500 dark:text-gray-300">
-        //                         Step 4: Connect to github.
-        //                         <Zoom>
-        //                             <img src={ConnectToGit} alt="Connect to github"/>
-        //                         </Zoom>
-        //                     </p>
-        //                     <p className="text-gray-500 dark:text-gray-300">
-        //                         Step 5: Choose a repository.
-        //                         <Zoom>
-        //                             <img src={ChooseRepository} alt="Choose repository"/>
-        //                         </Zoom>
-        //                     </p>
-        //                 </div>
+                            <hr/>
+                            <p className="text-gray-500 dark:text-gray-300">
+                                Step 1: Create your cloudfare account.
+                            </p>
+                            <p className="text-gray-500 dark:text-gray-300">
+                                Step 2: Create Page.
+                                <Zoom>
+                                    <img src={pageImage} alt="Cloud Fare page"/>
+                                </Zoom>
+                            </p>
+                            <p className="text-gray-500 dark:text-gray-300">
+                                Step 3: Create a project.
+                                <Zoom>
+                                    <img src={CreateProjectImage} alt="Create a project"/>
+                                </Zoom>
+                            </p>
+                            <p className="text-gray-500 dark:text-gray-300">
+                                Step 4: Connect to github.
+                                <Zoom>
+                                    <img src={ConnectToGit} alt="Connect to github"/>
+                                </Zoom>
+                            </p>
+                            <p className="text-gray-500 dark:text-gray-300">
+                                Step 5: Choose a repository.
+                                <Zoom>
+                                    <img src={ChooseRepository} alt="Choose repository"/>
+                                </Zoom>
+                            </p>
+                        </div>
 
-        //                 <div className="space-y-3">
-        //                     <h1 className="text-2xl font-semibold text-gray-700 capitalize dark:text-white">
-        //                        Continue...
-        //                     </h1>
-        //                     <hr/>
-        //                     <p className="text-gray-500 dark:text-gray-300">
-        //                         Step 6: Select a branch which you want to auto deploy.
-        //                         <Zoom>
-        //                             <img src={SelectBranch} alt="Select a Branch"/>
-        //                         </Zoom>
-        //                     </p>
-        //                     <p className="text-gray-500 dark:text-gray-300">
-        //                         Step 7: Select a branch which you want to auto deploy.
-        //                         <Zoom>
-        //                             <img src={SelectBranch} alt="Select a Branch"/>
-        //                         </Zoom>
-        //                     </p>
-        //                     <p className="text-gray-500 dark:text-gray-300">
-        //                         Step 8: Register your Domain name in the custom Domain panel.
-        //                         <Zoom>
-        //                             <img src={ChooseDomain} alt="Choose Domain "/>
-        //                         </Zoom>
-        //                     </p>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </section>
+                        <div className="space-y-3">
+                            <h1 className="text-2xl font-semibold text-gray-700 capitalize dark:text-white">
+                               Continue...
+                            </h1>
+                            <hr/>
+                            <p className="text-gray-500 dark:text-gray-300">
+                                Step 6: Select a branch which you want to auto deploy.
+                                <Zoom>
+                                    <img src={SelectBranch} alt="Select a Branch"/>
+                                </Zoom>
+                            </p>
+                            <p className="text-gray-500 dark:text-gray-300">
+                                Step 7: Select a branch which you want to auto deploy.
+                                <Zoom>
+                                    <img src={SelectBranch} alt="Select a Branch"/>
+                                </Zoom>
+                            </p>
+                            <p className="text-gray-500 dark:text-gray-300">
+                                Step 8: Register your Domain name in the custom Domain panel.
+                                <Zoom>
+                                    <img src={ChooseDomain} alt="Choose Domain "/>
+                                </Zoom>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
