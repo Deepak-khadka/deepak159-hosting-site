9eba2462f5d197a6d22b2c34a72597273b9ab27e
Deepak <rozankhdka159@gmail.com>
2022-06-30 22:43:56 +0545

    chore: remove target blank in portfolio

--- src/component/home/portfolio.js
+++ src/component/home/portfolio.js
@@ -41,21 +41,21 @@ const Portfolio = () => {
 
                   <div
                       className="mt-6 pb-16 lg:pb-0 w-4/5 lg:w-full mx-auto flex flex-wrap items-center justify-between">
-                      <a target="_blank" className="link" href="https://facebook.com/deepakkhadka.me" data-tippy-content="@facebook_handle">
+                      <a className="link" href="https://facebook.com/deepakkhadka.me" data-tippy-content="@facebook_handle">
                           <svg className="h-6 fill-current text-gray-600 hover:text-green-700" role="img"
                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Facebook</title>
                               <path
                                   d="M22.676 0H1.324C.593 0 0 .593 0 1.324v21.352C0 23.408.593 24 1.324 24h11.494v-9.294H9.689v-3.621h3.129V8.41c0-3.099 1.894-4.785 4.659-4.785 1.325 0 2.464.097 2.796.141v3.24h-1.921c-1.5 0-1.792.721-1.792 1.771v2.311h3.584l-.465 3.63H16.56V24h6.115c.733 0 1.325-.592 1.325-1.324V1.324C24 .593 23.408 0 22.676 0"/>
                           </svg>
                       </a>
-                      <a target="_blank" className="link" href="https://twitter.com/deepak_khadka15" data-tippy-content="@twitter_handle">
+                      <a className="link" href="https://twitter.com/deepak_khadka15" data-tippy-content="@twitter_handle">
                           <svg className="h-6 fill-current text-gray-600 hover:text-green-700" role="img"
                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Twitter</title>
                               <path
                                   d="M23.954 4.569c-.885.389-1.83.654-2.825.775 1.014-.611 1.794-1.574 2.163-2.723-.951.555-2.005.959-3.127 1.184-.896-.959-2.173-1.559-3.591-1.559-2.717 0-4.92 2.203-4.92 4.917 0 .39.045.765.127 1.124C7.691 8.094 4.066 6.13 1.64 3.161c-.427.722-.666 1.561-.666 2.475 0 1.71.87 3.213 2.188 4.096-.807-.026-1.566-.248-2.228-.616v.061c0 2.385 1.693 4.374 3.946 4.827-.413.111-.849.171-1.296.171-.314 0-.615-.03-.916-.086.631 1.953 2.445 3.377 4.604 3.417-1.68 1.319-3.809 2.105-6.102 2.105-.39 0-.779-.023-1.17-.067 2.189 1.394 4.768 2.209 7.557 2.209 9.054 0 13.999-7.496 13.999-13.986 0-.209 0-.42-.015-.63.961-.689 1.8-1.56 2.46-2.548l-.047-.02z"/>
                           </svg>
                       </a>
-                      <a target="_blank" className="link" href="https://github.com/Deepak-khadka" data-tippy-content="@github_handle">
+                      <a className="link" href="https://github.com/Deepak-khadka" data-tippy-content="@github_handle">
                           <svg className="h-6 fill-current text-gray-600 hover:text-green-700" role="img"
                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title>
                               <path
@@ -63,14 +63,14 @@ const Portfolio = () => {
                           </svg>
                       </a>
 
-                      <a target="_blank" className="link" href="https://dribbble.com/deepak_khadka159" data-tippy-content="@dribble_handle">
+                      <a  className="link" href="https://dribbble.com/deepak_khadka159" data-tippy-content="@dribble_handle">
                           <svg className="h-6 fill-current text-gray-600 hover:text-green-700" role="img"
                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Dribbble</title>
                               <path
                                   d="M12 24C5.385 24 0 18.615 0 12S5.385 0 12 0s12 5.385 12 12-5.385 12-12 12zm10.12-10.358c-.35-.11-3.17-.953-6.384-.438 1.34 3.684 1.887 6.684 1.992 7.308 2.3-1.555 3.936-4.02 4.395-6.87zm-6.115 7.808c-.153-.9-.75-4.032-2.19-7.77l-.066.02c-5.79 2.015-7.86 6.025-8.04 6.4 1.73 1.358 3.92 2.166 6.29 2.166 1.42 0 2.77-.29 4-.814zm-11.62-2.58c.232-.4 3.045-5.055 8.332-6.765.135-.045.27-.084.405-.12-.26-.585-.54-1.167-.832-1.74C7.17 11.775 2.206 11.71 1.756 11.7l-.004.312c0 2.633.998 5.037 2.634 6.855zm-2.42-8.955c.46.008 4.683.026 9.477-1.248-1.698-3.018-3.53-5.558-3.8-5.928-2.868 1.35-5.01 3.99-5.676 7.17zM9.6 2.052c.282.38 2.145 2.914 3.822 6 3.645-1.365 5.19-3.44 5.373-3.702-1.81-1.61-4.19-2.586-6.795-2.586-.825 0-1.63.1-2.4.285zm10.335 3.483c-.218.29-1.935 2.493-5.724 4.04.24.49.47.985.68 1.486.08.18.15.36.22.53 3.41-.43 6.8.26 7.14.33-.02-2.42-.88-4.64-2.31-6.38z"/>
                           </svg>
                       </a>
-                      <a target="_blank" className="link" href="https://www.instagram.com/dpk_khadka/" data-tippy-content="@instagram_handle">
+                      <a  className="link" href="https://www.instagram.com/dpk_khadka/" data-tippy-content="@instagram_handle">
                           <svg className="h-6 fill-current text-gray-600 hover:text-green-700" role="img"
                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Instagram</title>
                               <path
