7c4466e70dfe6264f6a2d4ba8f5777dd8fc56589
Deepak <rozankhdka159@gmail.com>
2022-07-05 07:17:27 +0545

    chore: include laravel tips and tricks part 1

--- src/component/BlogList/LaravelTipsAndTricksPart1.js
+++ src/component/BlogList/LaravelTipsAndTricksPart1.js
@@ -2,28 +2,30 @@ import ToSql from "../../images/laravel-tips-and-trics/toSql.png";
 import Zoom from "react-medium-image-zoom";
 
 const LaravelTipsAndTricksPart1 = () => {
-  return (
-    <>
-      <section className="bg-white dark:bg-gray-900 container-fluid">
-        <div className="container px-6 py-10 mx-auto">
+    return (
+        <>
+            <section className="bg-white dark:bg-gray-900 container-fluid">
+                <div className="container px-6 py-10 mx-auto">
 
-        <div className="mt-8 xl:mt-5 lg:flex lg:items-center">
-                    <div className="w-full grid grid-cols-2 columns-3 gap-8">
+                    <div className="mt-8 xl:mt-5 lg:flex lg:items-center">
+                        <div className="w-full grid grid-cols-2 columns-3 gap-8">
 
-          <p className="text-gray-500 dark:text-gray-300 bg-gray-700">
-            <code>Note: The toQuery method returns an Eloquent query builder 
-                instance containing a whereIn constraint on the collection model's primary keys:</code>
-            <Zoom>
-              <img src={ToSql} alt="Cloud Fare page"/>
-            </Zoom>
-          </p>
+                            <p className="bg-gray-800">
+                                <span className="text-gray-400 p-3">Note: The toQuery
+                                    method returns an Eloquent query builder instance containing a whereIn constraint on the collection model's primary
+                                    keys:</span>
 
-          </div>
-          </div>
-        </div>
-      </section>
-    </>
-  );
+                                <Zoom>
+                                    <img src={ToSql} alt="Cloud Fare page"/>
+                                </Zoom>
+
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 };
 
 export default LaravelTipsAndTricksPart1;
